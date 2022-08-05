fc542791d127c20711e53f81ac31ca046ce027bf
Valeria <mukhina_lera@list.ru>
2022-08-05 14:40:20 +0300

    .\Example0304_Div\

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numderA = 3 ;
-int numderB = 55 ;
+﻿int numderA = new Random().Next (1,10) ; // 1 2 3 4 .. 9
+Console.WriteLine(numderA);
+int numderB =  new Random().Next (1,10);
+Console.WriteLine(numderB);
 int result = (numderA + numderB);
 Console.WriteLine(result);
